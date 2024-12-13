471295474dfd1ea8cdde6fa8e032ff175716ad69
Woo3574 <rlaxodn0429@gmail.com>
2024-12-13 21:48:40 +0900

    24.12.13

--- kh_2tp_react/src/components/MobileHome.js
+++ kh_2tp_react/src/components/MobileHome.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import { useState, useMemo, useRef } from "react";
 import { Link } from "react-router-dom";
-import { StarTwoTone } from "@ant-design/icons";
+
+
 
 // 두 위도-경도 좌표 간의 거리를 계산하는 Haversine 공식
 const calculateDistance = (lat1, lon1, lat2, lon2) => {
@@ -30,7 +31,6 @@ const calculateDistance = (lat1, lon1, lat2, lon2) => {
 };
 
 const MobileHomeItemBlock = styled.div`
-  @media (max-width: 768px) {
     width: 100%;
     max-width: 768px; /* 너비를 768px로 고정 */
     margin: 0 auto; /* 화면 중앙 정렬 */
@@ -39,17 +39,15 @@ const MobileHomeItemBlock = styled.div`
     align-items: center;
     flex-direction: column;
     background-color: #fff;
-  }
+    min-width: 680px; /* 최소 너비 640px로 설정 */
 `;
 
 const DropdownContainer = styled.div`
-  @media (max-width: 768px) {
     width: 100%;
     height: 30px;
     display: flex;
     justify-content: right;
     position: relative;
-  }
 `;
 
 const Dropdown = styled.select`
@@ -106,7 +104,7 @@ const BrandContainer = styled.div`
 
 const BrandMain = styled.div`
   box-sizing: border-box;
-  width: 201px; /* 부모 요소 너비에 맞게 조정 */
+  width: 400px; /* 부모 요소 너비에 맞게 조정 */
   height: 38px; /* 고정 높이 */
   margin-top: 10px;
   margin-bottom: 10px;
@@ -120,6 +118,8 @@ const BrandMain = styled.div`
 `;
 
 const NameBar = styled.div`
+  width: 100%;
+  margin-left: 80px;
   display: flex;
   flex-direction: row;
 `;
@@ -127,6 +127,7 @@ const NameBar = styled.div`
 const BrandFontBox = styled.div`
   display: flex;
   flex-direction: column;
+ 
 `;
 
 const BrandName = styled.div`
@@ -143,7 +144,10 @@ const BrandDesc = styled.div`
   width: 100%;
   height: 100%;
   padding-left: 5px;
-  font-size: 12px;
+  font-size: 10px;
+  color: #a1a1a1;
+  display: flex;
+  justify-items:start;
 `;
 
 const StoresContainer = styled.div`
@@ -179,7 +183,6 @@ const ArrowButton = styled.button`
 const Stores = styled.div`
   box-sizing: border-box;
   display: flex;
-  width: 660px;
   white-space: nowrap;
   scroll-behavior: smooth;
   overflow-x: hidden;
@@ -251,6 +254,32 @@ const MobileHome = ({ mobileDataReceivedAfterSearch }) => {
   const [sortType, setSortType] = useState("name");
   const [sortByDistance, setSortByDistance] = useState(false);
   const containerRefs = useRef([]);
+  console.log(mobileDataReceivedAfterSearch); // 데이터 확인
+
+
+   // 브랜드 이름과 설명을 매핑한 배열
+   const brandDescriptions = [
+    { brandName: "빽보이피자", description: "TRUST ME, THIS IS THE BEST!" },
+    { brandName: "역전우동", description: "백종원의 우동&덮밥 전문점" },
+    { brandName: "빽다방", description: "합리적인 커피 문화를 만들어 갑니다." },
+    { brandName: "홍콩반점", description: "대한민국 No.1 중식 브랜드" },
+    { brandName: "롤링파스타", description: "이탈리안 파스타의 캐주얼한 해석" },
+    { brandName: "한신포차", description: "대한민국 실내 포장마차" },
+    { brandName: "백스비어", description: "Delicious Food&Beer" },
+    { brandName: "새마을식당", description: "백종원의 열탄불고기 전문점점" },
+    { brandName: "제순식당", description: "소박하지만 돌아서면 생각나는 따뜻한 맛" },
+    { brandName: "리춘시장", description: "대한민국 No.1 중식 주점점 브랜드" },
+    { brandName: "고투웍", description: "아메리칸 차이니즈 푸드" },
+    { brandName: "홍콩분식", description: "홍콩반점의 노하우를 담은 떡볶이 브랜드" },
+    { brandName: "백종원의쌈밥", description: "20여가지 신선한 채소로 즐기는 건강한 한 상!" },
+    { brandName: "본가", description: "한식의 세계화에 앞장 서는 우삼겹의 원조" },
+    { brandName: "인생설렁탕", description: "인생에 기억될 맛, 인생을 추억할 맛" },
+    { brandName: "막이오름", description: "우리술의 새로운 막이 오르다" },
+    { brandName: "연돈볼카츠", description: "제주 연돈의 노하우와 우리 돼지 한돈의 꽉 찬 만남" },
+    { brandName: "돌배기집", description: "백종원의 차돌박이 전문점점" },
+    { brandName: "미정국수", description: "오랜시간 정성으로 만들어낸 한 그릇의 국수" },
+    { brandName: "백철판판", description: "여러가지 식재료로 만드는 다양한 철판요리" },
+  ];
 
   const setRef = (index) => (element) => {
     containerRefs.current[index] = element; // 해당 인덱스에 DOM 요소 할당
@@ -346,6 +375,14 @@ const MobileHome = ({ mobileDataReceivedAfterSearch }) => {
     }
   };
 
+    // 해당 브랜드 이름에 맞는 설명을 찾는 함수
+    const getBrandDescription = (brandName) => {
+      const brand = brandDescriptions.find(
+        (item) => item.brandName === brandName
+      );
+      return brand ? brand.description : "설명 없음"; // 기본값은 '설명 없음'
+    };
+
   return (
     <>
       <MobileHomeItemBlock>
@@ -375,7 +412,7 @@ const MobileHome = ({ mobileDataReceivedAfterSearch }) => {
                             {brandData.brand.brandName} {/* 브랜드 이름 표시 */}
                           </BrandName>
                           <BrandDesc>
-                          <p>1</p>
+                            {getBrandDescription(brandData.brand.brandName)} {/* 브랜드 설명 표시 */}
                           </BrandDesc>
                         </BrandFontBox>
                     </NameBar>
@@ -392,33 +429,33 @@ const MobileHome = ({ mobileDataReceivedAfterSearch }) => {
                   <Stores ref={setRef(index)}>
                     {brandData.stores.map((store) => (
                       <StyledLink to={`/stores/${store.storeNo}`} key={store.storeNo}>
-                        <EachStore>
-                          <EachImage
-                            style={{
-                              backgroundImage: `url(${brandData.brand.brandImg1})`,
-                            }}
-                          ></EachImage>
-                          <EachTextContainer>
-                            <EachText1>{store.storeName}</EachText1>
-                            <EachText2>
-                              <p style={{ color: "RED", display: "inline" }}>
-                                ★{" "}
-                              </p>
-                              <p style={{ display: "inline" }}>
-                                {store.avgRatingVO.averageRating}
-                              </p>
-                              <p
-                                style={{
-                                  color: "#a4a4a4",
-                                  display: "inline",
-                                  marginLeft: "5px",
-                                }}
-                              >
-                                {store.brandVO.brandFood}ㆍ{store.storeAddr}
-                              </p>
-                            </EachText2>
-                          </EachTextContainer>
-                        </EachStore>
+                          <EachStore>
+                            <EachImage
+                              style={{
+                                backgroundImage: `url(${brandData.brand.brandImg1})`,
+                              }}
+                            ></EachImage>
+                            <EachTextContainer>
+                              <EachText1>{store.storeName}</EachText1>
+                              <EachText2>
+                                <p style={{ color: "RED", display: "inline" }}>
+                                  ★{" "}
+                                </p>
+                                <p style={{ display: "inline" }}>
+                                  {store.avgRatingVO.averageRating}
+                                </p>
+                                <p
+                                  style={{
+                                    color: "#a4a4a4",
+                                    display: "inline",
+                                    marginLeft: "5px",
+                                  }}
+                                >
+                                  {store.brandVO.brandFood}ㆍ{store.storeAddr}
+                                </p>
+                              </EachText2>
+                            </EachTextContainer>
+                          </EachStore>
                       </StyledLink>
                     ))}
                   </Stores>
--- kh_2tp_react/src/styles/GlobalStyle.js
+++ kh_2tp_react/src/styles/GlobalStyle.js
@@ -8,7 +8,6 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-    
   }
 `;
 
